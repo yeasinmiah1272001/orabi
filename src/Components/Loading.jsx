a293093b72d8b6d0b9900b60568b0bca78987366
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2024-11-19 11:36:51 +0600

    added

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaSpinner } from "react-icons/fa";
 
 const Loading = () => {
@@ -6,7 +5,9 @@ const Loading = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <FaSpinner className="text-blue-500 text-6xl animate-spin mb-4" />
       <h2 className="text-xl font-semibold text-gray-700">Loading...</h2>
-      <p className="text-gray-500">Please wait while we load the data.</p>
+      <p className="text-gray-500">
+        Please wait while we load the data data.....
+      </p>
     </div>
   );
 };
